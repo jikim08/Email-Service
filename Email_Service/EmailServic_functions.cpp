9724ea814428e771588bc4d281724b5e5209260a
Jihoo <jikim08@naver.com>
2022-11-30 14:31:00 +0900

    주석 추가

--- Email_Service/EmailServic_functions.cpp
+++ Email_Service/EmailServic_functions.cpp
@@ -7,8 +7,8 @@
 using namespace std;
 
 void MainController::start() {
-	AccountController IDlist;
-	int cmd;
+	AccountController IDlist;							//계정 저장소
+	int cmd;								//메뉴 입력용
 
 	while (true) {
 		system("cls");
@@ -22,11 +22,11 @@ void MainController::start() {
 			return;
 
 		else if (cmd == 1) {
-			IDlist.SignUp();
+			IDlist.SignUp();			//회원가입
 		}
 
 		else if (cmd == 2) {
-			EmailController& loginID = IDlist.Login();
+			EmailController& loginID = IDlist.Login();		//현재 로그인한 계정
 			while (true) {
 				system("cls");
 				cout << "로그아웃 0\n";
@@ -38,7 +38,7 @@ void MainController::start() {
 					break;
 				}
 				else if (cmd == 1) {
-					loginID.Send(IDlist);
+					loginID.Send(IDlist);					//메일 보내기
 					cout << "전송완료\n";
 					Sleep(1000);
 				}
@@ -52,6 +52,10 @@ void MainController::start() {
 					loginID.ShowReceive();
 					system("pause");
 				}
+				else {
+					cout << "잘못된 메뉴입니다\n";
+					Sleep(1000);
+				}
 			}
 		}
 
@@ -60,7 +64,7 @@ void MainController::start() {
 		}
 		else{
 			cout << "잘못된 메뉴입니다." << endl;
-			Sleep(2000);
+			Sleep(1000);
 		}
 
 	}
@@ -75,13 +79,13 @@ void AccountController::SignUp() {
 		cout << "PW: ";
 		cin >> PW;
 
-		if (IDPW.find(ID) != IDPW.end()) {
+		if (IDPW.find(ID) != IDPW.end()) {				//ID가 이미 있는 경우
 			cout << "이미 있는 ID입니다" << endl;
 			Sleep(2000);
 		}
 		else {
-			IDPW.insert({ ID,PW });
-			data.insert({ ID,EmailController(ID) });
+			IDPW.insert({ ID,PW });						//ID와 PW를 맵에 저장
+			data.insert({ ID,EmailController(ID) });	//ID와 해당 계정 정보를 맵에 저장
 			cout << ID << "님, 가입이 완료되었습니다\n" << endl;
 			Sleep(2000);
 			break;
@@ -121,7 +125,7 @@ EmailController& AccountController::Login() {
 		else {
 			cout << ID << "님, 환영합니다\n\n";
 			Sleep(1500);
-			return data.find(ID)->second;
+			return data.find(ID)->second;					//로그인 한 계정(참조값)을 리턴
 
 		}
 
@@ -130,21 +134,22 @@ EmailController& AccountController::Login() {
 }
 void EmailController::Send(AccountController& acclist) {
 	Emailstorage mail;
-
+	
 	while (true) {
 		system("cls");
 		cout << "To: ";
 		cin >> mail.To;
 		cout << "Title: ";
-		cin >> mail.Title;
+		cin.ignore();				//엔터 무시
+		getline(cin, mail.Title);
 		cout << "Contents: ";
-		cin >> mail.Contents;
+		getline(cin, mail.Contents);
 
 		mail.From = ID;
 		
-		bool flag = acclist.sending(mail.To, mail);
+		bool flag = acclist.sending(mail.To, mail);			//수신자 유무 리턴, 수신자가 있으면 수신자 받은 메일함에 메일 저장
 		
-		if (!flag) {
+		if (!flag) {			//수신자 ID가 존재하지 않는 경우
 			continue;
 		}
 		
@@ -155,16 +160,17 @@ void EmailController::Send(AccountController& acclist) {
 	
 	
 }
-void EmailController::Receive(Emailstorage mail) {
+void EmailController::Receive(Emailstorage mail) {			//receiveMail에 접근하기 위한 함수
 	receiveMail.push_back(mail);
 }
 
-bool AccountController::sending(string ID, Emailstorage mail) {
+bool AccountController::sending(string ID, Emailstorage mail) {		//수신자 유무 리턴, 수신자가 있으면 수신자 받은 메일함에 메일 저장
 	if (data.find(ID) == data.end()) {
 		cout << "수신자가 존재하지 않습니다\n";
 		Sleep(1000);
 		return false;
 	}
+
 	data.find(ID)->second.Receive(mail);
 	return true;
 }
--- Email_Service/EmailService.h
+++ Email_Service/EmailService.h
@@ -14,32 +14,33 @@ public:
 
 class AccountController {
 private:
-	map<string, string> IDPW;
-	map<string, EmailController> data;
+	map<string, string> IDPW;				//ID 비밀번호 저장
+	map<string, EmailController> data;		//ID에 따른 메일목록 저장
 public:
-	void SignUp();
-	void ShowIDs();
-	EmailController& Login();
-	bool sending(string ID, Emailstorage mail);
+	void SignUp();							//회원가입
+	void ShowIDs();							//ID들 보기
+	EmailController& Login();				//로그인하기
+	bool sending(string ID, Emailstorage mail);	//메일 전송 중간과정(data가 이 클래스에 있으므로 data에 접근하기 위한 용도)
 };
 
-class Emailstorage {
+class Emailstorage {						//메일 한개의 정보를 저장할 수 있는 구조체
 public:
 	string To;
 	string From;
 	string Title;
 	string Contents;
 };
+
 class EmailController {
 private:
-	string ID;
-	vector<Emailstorage> sendMail;
-	vector<Emailstorage> receiveMail;
+	string ID;								//해당 객체의 ID
+	vector<Emailstorage> sendMail;			//보낸 메일
+	vector<Emailstorage> receiveMail;		//받은 메일
 public:
-	EmailController();
-	EmailController(string ID);
-	void Send(AccountController& acclist);
-	void Receive(Emailstorage mail);
+	EmailController();						//디폴트	생성자
+	EmailController(string ID);				//ID를 저장해주는 생성자
+	void Send(AccountController& acclist);	//보내기(메일 입력 및 보내기)
+	void Receive(Emailstorage mail);		//받기(AccountEmail클래스에서 receivemail에 접근하기 위한 함수)
 	void ShowSend();
 	void ShowReceive();
 
